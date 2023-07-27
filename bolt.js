16416f07927de3d3584e32e9d38d9eb4e09bc183
ir. Jos Deliën M.Sc <josdelien@gmail.com>
2023-07-27 23:12:20 +0200

    Create bolt.js
    initial functionality

---
+++ bolt.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  const invokeTauriCommand = async function(cmd) {
+    if (window.__TAURI__) {
+      const { invoke } = window.__TAURI__.tauri;
+      return await invoke(cmd);
+    }
+    return null;
+  };
+
+  const handleBoltCmd = async function(event) {
+    const targetElement = event.target;
+    const boltCmd = targetElement.getAttribute("bolt-cmd");
+    if (boltCmd) {
+      const boltTrigger = targetElement.getAttribute("bolt-trigger");
+      if (boltTrigger === "load" || !boltTrigger) {
+        try {
+          const response = await invokeTauriCommand(boltCmd);
+          const boltSwap = targetElement.getAttribute("bolt-swap");
+          const boltTarget = targetElement.getAttribute("bolt-target");
+          let target = boltTarget ? document.querySelector(boltTarget) : targetElement;
+
+          switch (boltSwap) {
+            case 'innerHTML':
+              target.innerHTML = response;
+              break;
+            case 'afterend':
+              target.insertAdjacentHTML("afterend", response);
+              break;
+            case 'outerHTML':
+              target.outerHTML = response;
+              break;
+            default:
+              console.warn(`Unknown bolt-swap value: ${boltSwap}`);
+          }
+        } catch (error) {
+          console.error("Error invoking Tauri command:", error);
+        }
+      }
+    }
+  };
+
+  // Init
+  document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("click", handleBoltCmd);
+  });
+
+})();

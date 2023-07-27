641910d5009ff3aaf48b1c13987b10c42f976c83
ir. Jos Deliën M.Sc <josdelien@gmail.com>
2023-07-28 01:45:23 +0200

    refactor bolt.js

--- bolt.js
+++ bolt.js
@@ -9,41 +9,38 @@
     return null;
   };
 
-  const handleBoltCmd = async function(event) {
+  const handleBoltAction = async function(event) {
     const targetElement = event.target;
     const boltCmd = targetElement.getAttribute("bolt-cmd");
     if (boltCmd) {
-      const boltTrigger = targetElement.getAttribute("bolt-trigger");
-      if (boltTrigger === "load" || !boltTrigger) {
-        try {
-          const response = await invokeTauriCommand(boltCmd);
-          const boltSwap = targetElement.getAttribute("bolt-swap");
-          const boltTarget = targetElement.getAttribute("bolt-target");
-          let target = boltTarget ? document.querySelector(boltTarget) : targetElement;
+      try {
+        const response = await invokeTauriCommand(boltCmd);
+        const boltSwap = targetElement.getAttribute("bolt-swap");
+        const boltTarget = targetElement.getAttribute("bolt-target");
+        let target = boltTarget ? document.querySelector(boltTarget) : targetElement;
 
-          switch (boltSwap) {
-            case 'innerHTML':
-              target.innerHTML = response;
-              break;
-            case 'afterend':
-              target.insertAdjacentHTML("afterend", response);
-              break;
-            case 'outerHTML':
-              target.outerHTML = response;
-              break;
-            default:
-              console.warn(`Unknown bolt-swap value: ${boltSwap}`);
-          }
-        } catch (error) {
-          console.error("Error invoking Tauri command:", error);
+        switch (boltSwap) {
+          case 'innerHTML':
+            target.innerHTML = response;
+            break;
+          case 'afterend':
+            target.insertAdjacentHTML("afterend", response);
+            break;
+          case 'outerHTML':
+            target.outerHTML = response;
+            break;
+          default:
+            console.warn(`Unknown bolt-swap value: ${boltSwap}`);
         }
+      } catch (error) {
+        console.error("Error invoking Tauri command:", error);
       }
     }
   };
 
   // Init
   document.addEventListener("DOMContentLoaded", function() {
-    document.body.addEventListener("click", handleBoltCmd);
+    document.body.addEventListener("click", handleBoltAction);
   });
 
 })();

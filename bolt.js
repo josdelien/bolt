6d6728d6836c6d0953ec18d6d33b41429a471506
ir. Jos Deliën M.Sc <josdelien@gmail.com>
2023-07-28 02:46:42 +0200

    implemented bolt-swap="remove <all>"

--- bolt.js
+++ bolt.js
@@ -12,13 +12,14 @@
   const handleBoltAction = async function(event) {
     const targetElement = event.target;
     const boltCmd = targetElement.getAttribute("bolt-cmd");
+    const boltSwap = targetElement.getAttribute("bolt-swap");
+    const boltTarget = targetElement.getAttribute("bolt-target");
+    let target = boltTarget ? document.querySelector(boltTarget) : targetElement;
+
+    // If bolt-cmd is present, execute the Tauri command
     if (boltCmd) {
       try {
         const response = await invokeTauriCommand(boltCmd);
-        const boltSwap = targetElement.getAttribute("bolt-swap");
-        const boltTarget = targetElement.getAttribute("bolt-target");
-        let target = boltTarget ? document.querySelector(boltTarget) : targetElement;
-
         switch (boltSwap) {
           case 'innerHTML':
             target.innerHTML = response;
@@ -29,12 +30,21 @@
           case 'outerHTML':
             target.outerHTML = response;
             break;
-          default:
-            console.warn(`Unknown bolt-swap value: ${boltSwap}`);
         }
       } catch (error) {
         console.error("Error invoking Tauri command:", error);
       }
+    }
+
+    // Handle DOM-only actions regardless of bolt-cmd presence
+    switch (boltSwap) {
+      case 'remove':
+        target.remove();
+        break;
+      case 'remove all':
+        const elementsToRemove = document.querySelectorAll(boltTarget);
+        elementsToRemove.forEach(el => el.remove());
+        break;
     }
   };
 
